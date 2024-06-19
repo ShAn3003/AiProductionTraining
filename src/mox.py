e2aa5f2e212298535270568bcf89cad9598e6faa
shan3003 <1292210712x@gmail.com>
2024-06-19 13:11:41 +0800

    train_in_modelart

--- src/mox.py
+++ src/mox.py
@@ -2,8 +2,8 @@ import moxing as mox
 
 # src 起点 dst 目的地    from obs
 
-src_url = "obs://computer-vision-lungtest/lung_data.zip"
-dst_url = "./lung"
+src_url = "/home/ma-user/work/AiProductionTraining/src/LungNudulesDetect/10_epoches_test10/weights/last.pt"
+dst_url = "obs://lungnudulesdata/yolov8.pt"
 
 mox.file.copy_parallel(src_url, dst_url)
 
